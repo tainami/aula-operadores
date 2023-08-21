b008cf1766ba876192d7a24133fca8d9a1fd144f
Tainá <121645958+tainami@users.noreply.github.com>
2023-08-21 20:05:10 -0300

    Adicionando exercícios

--- desafio.js
+++ desafio.js
@@ -1,21 +1,17 @@
-/*
-- Graus Fahrenheit(°F) para Kelvin(K)
-    
-    ```
-    (KELVIN) = (GRAUS_FAHRENHEIT - 32)*(5/9) + 273.15
-    ```
-    
-- Graus Celsius(°C) para Graus Fahrenheit (°C)
-
-a) Calcule e mostre o valor de 77°F em  K, mostrando a unidade no console também.
-
-b) Calcule e mostre o valor de 80°C em °F, mostrando a unidade no console também
-
-c) Calcule e mostre o valor de 30°C em °F e K, mostrando as unidades no console também
-
-d) Altere o último item para que o usuário insira o valor em graus Celsius que ele deseja converter
-*/
+// 1
 
 let celsius = 0
 let fahrenheit = celsius *(9/5) + 32
 let kelvin = ((fahrenheit - 32) * (5/9)) + 273.15
+
+fahrenheit = 77 
+console.log("77°F em  K é", kelvin + "°K")
+
+celsius = 80
+fahrenheit = celsius *(9/5) + 32
+console.log("80°C em °F é", fahrenheit + "°F")
+
+celsius = 30
+fahrenheit = celsius *(9/5) + 32
+kelvin = ((fahrenheit - 32) * (5/9)) + 273.15
+console.log("30°C em °F e K é", fahrenheit + "°F", "e", kelvin + "°K")
